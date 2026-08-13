0c2dda92968b03bc2cd41f112de722c488213077
taeminlee25 <108789989+taeminlee25@users.noreply.github.com>
2026-08-13 22:02:33 +0900

    테스트와 DI

--- src/test/java/org/example/tobyspring/payment/PaymentServiceSpringTest.java
+++ src/test/java/org/example/tobyspring/payment/PaymentServiceSpringTest.java
@@ -19,19 +19,30 @@ import static java.math.BigDecimal.TEN;
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SuppressWarnings("ALL")
 @ExtendWith(SpringExtension.class)
 // 테스트 오브젝트 팩토리를 이용하여 스프링 컨테이너를 구성
 @ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
     // 컨테이너는 자기 자신도 가져올 수 있다
     @Autowired PaymentService paymentService;
+    @Autowired ExRateProviderStub exRateProviderStub;
 
     @Test
     @DisplayName("prepare 메서드가 요구사항 3가지를 잘 충족했는지 검증")
     void convertedAmount() throws IOException {
+        // exRate: 1000
         Payment payment = paymentService.prepare(1L, "USD", TEN);
 
         assertThat(payment.getExRate()).isEqualTo(valueOf(1_000));
         assertThat(payment.getConvertedAmount()).isEqualByComparingTo(valueOf(10_000));
+
+        // exRate: 500
+        exRateProviderStub.setExRate(valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", TEN);
+
+        assertThat(payment2.getExRate()).isEqualTo(valueOf(500));
+        assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
+
     }
 }
